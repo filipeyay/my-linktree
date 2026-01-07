eb00ea906445de800fda41a2a126f792cdc0d6f7
Filipe Andrade <filipeyay@gmail.com>
2026-01-07 16:59:16 -0300

    fixed build problems

--- src/main/java/com/linktree/mylinktree/controllers/HomeController.java
+++ src/main/java/com/linktree/mylinktree/controllers/HomeController.java
@@ -1,21 +1,12 @@
-package com.linktree.mylinktree.controller;
+package com.linktree.mylinktree.controllers;
 
-import com.linktree.mylinktree.service.LinkService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    private final LinkService linkService;
-
-    public HomeController(LinkService linkService) {
-        this.linkService = linkService;
-    }
-
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("links", linkService.getLinks());
+    public String index() {
         return "index";
     }
 }
